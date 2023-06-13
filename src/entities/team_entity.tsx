7d80018f188785611f56c40727d8f3cb71a9909b
mendesisis <vantaeisis@gmail.com>
2023-06-13 20:54:02 -0300

    13_06

---
+++ src/entities/team_entity.tsx
@@ -0,0 +1,9 @@
+    export default class TeamEntity {
+        id: number; 
+        team_position: number;
+        team_name: string;
+        team_shield: string;
+        team_points: number;
+     
+
+    }
